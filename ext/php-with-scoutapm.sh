211b9247c13553195d02ddef7c95114012b83741
James Titcumb <james@asgrim.com>
2019-08-26 09:36:51 +0100

    Added SO for PHP compiled in ZTS mode

--- ext/php-with-scoutapm.sh
+++ ext/php-with-scoutapm.sh
@@ -4,9 +4,19 @@ set -euxo pipefail
 
 cd "$(dirname "$0")/../"
 
+PHP_PATH=$(which php)
+
 WORKING_DIRECTORY=$(pwd)
-PHP_API=$(php -i | grep 'PHP API' | sed -e 's/PHP API => //')
-EXTPATH="$WORKING_DIRECTORY/ext/scoutapm-$PHP_API.so"
+PHP_API=$($PHP_PATH -i | grep 'PHP API' | sed -e 's/PHP API => //')
+THREAD_SAFETY=$($PHP_PATH -i | grep 'Thread Safety' | sed -e 's/Thread Safety => //')
+
+if [ "$THREAD_SAFETY" == "enabled" ]; then
+  ZTS_FLAG="-zts"
+else
+  ZTS_FLAG=""
+fi
+
+EXTPATH="$WORKING_DIRECTORY/ext/scoutapm-$PHP_API$ZTS_FLAG.so"
 
 if [[ ! -f $EXTPATH ]]; then
   echo "Extension was not found for PHP API $PHP_API (at $EXTPATH)"
@@ -14,6 +24,7 @@ if [[ ! -f $EXTPATH ]]; then
 fi
 
 echo "PHP API version is: $PHP_API"
+echo "Thread safety is: $THREAD_SAFETY"
 echo "Extension path is: $EXTPATH"
 
-php -d zend_extension=$EXTPATH "$@"
+$PHP_PATH -d zend_extension=$EXTPATH "$@"
