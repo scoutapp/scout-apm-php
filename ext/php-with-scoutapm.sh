803f6bf97c88dc1bd480cf01ef439bb1c5471f93
James Titcumb <james@asgrim.com>
2019-09-09 12:23:18 +0100

    Build extension from latest source until PECL is available

--- ext/php-with-scoutapm.sh
+++ ext/php-with-scoutapm.sh
@@ -6,26 +6,7 @@ cd "$(dirname "$0")/../"
 
 PHP_PATH=$(which php)
 
-WORKING_DIRECTORY=$(pwd)
-PHP_API=$($PHP_PATH -i | grep 'PHP API' | sed -e 's/PHP API => //')
-THREAD_SAFETY=$($PHP_PATH -i | grep 'Thread Safety' | sed -e 's/Thread Safety => //')
-
-if [ "$THREAD_SAFETY" == "enabled" ]; then
-  ZTS_FLAG="-zts"
-else
-  ZTS_FLAG=""
-fi
-
-EXTPATH="$WORKING_DIRECTORY/ext/scoutapm-$PHP_API$ZTS_FLAG.so"
-
-if [[ ! -f $EXTPATH ]]; then
-  echo "Extension was not found for PHP API $PHP_API (at $EXTPATH)"
-  exit 1
-fi
-
-echo "PHP API version is: $PHP_API"
-echo "Thread safety is: $THREAD_SAFETY"
-echo "Extension path is: $EXTPATH"
+EXTPATH=scoutapm.so
 
 $PHP_PATH -d zend_extension=$EXTPATH -r "if (!extension_loaded('scoutapm')) { echo 'ERROR - Scout APM extension failed to load' . PHP_EOL; } exit(extension_loaded('scoutapm')?0:1);"
 
