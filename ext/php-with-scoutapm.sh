1575bb67772e80e8df259632d08f56c4d9f9efe2
James Titcumb <james@asgrim.com>
2019-09-11 13:48:31 +0100

    scout-apm-php-ext is now a regular PHP module, not a zend_extension

--- ext/php-with-scoutapm.sh
+++ ext/php-with-scoutapm.sh
@@ -8,6 +8,6 @@ PHP_PATH=$(which php)
 
 EXTPATH=scoutapm.so
 
-$PHP_PATH -d zend_extension=$EXTPATH -r "if (!extension_loaded('scoutapm')) { echo 'ERROR - Scout APM extension failed to load' . PHP_EOL; } exit(extension_loaded('scoutapm')?0:1);"
+$PHP_PATH -d extension=$EXTPATH -r "if (!extension_loaded('scoutapm')) { echo 'ERROR - Scout APM extension failed to load' . PHP_EOL; } exit(extension_loaded('scoutapm')?0:1);"
 
-$PHP_PATH -d zend_extension=$EXTPATH "$@"
+$PHP_PATH -d extension=$EXTPATH "$@"
