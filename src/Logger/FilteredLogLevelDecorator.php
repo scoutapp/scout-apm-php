b89a37d521473f227a1ef685ca762e4513164607
James Titcumb <james@asgrim.com>
2019-10-02 10:50:30 +0100

    Added logger implementation that filters noisy log levels by configuration

---
+++ src/Logger/FilteredLogLevelDecorator.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scoutapm\Logger;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Psr\Log\LogLevel;
+use function strtolower;
+
+/**
+ * This log decorator is used to squelch log messages below a configured threshold.
+ */
+final class FilteredLogLevelDecorator implements LoggerInterface
+{
+    use LoggerTrait;
+
+    private const LOG_LEVEL_ORDER = [
+        LogLevel::DEBUG => 0,
+        LogLevel::INFO => 1,
+        LogLevel::NOTICE => 2,
+        LogLevel::WARNING => 3,
+        LogLevel::ERROR => 4,
+        LogLevel::CRITICAL => 5,
+        LogLevel::ALERT => 6,
+        LogLevel::EMERGENCY => 7,
+    ];
+
+    /** @var LoggerInterface */
+    private $realLogger;
+
+    /** @var int */
+    private $minimumLogLevel;
+
+    /**
+     * @param string $minimumLogLevel e.g. `emergency`, `error`, etc. - {@see \Psr\Log\LogLevel}
+     */
+    public function __construct(LoggerInterface $realLogger, string $minimumLogLevel)
+    {
+        $this->minimumLogLevel = self::LOG_LEVEL_ORDER[strtolower($minimumLogLevel)];
+        $this->realLogger      = $realLogger;
+    }
+
+    /** {@inheritDoc} */
+    public function log($level, $message, array $context = [])
+    {
+        if ($this->minimumLogLevel > self::LOG_LEVEL_ORDER[$level]) {
+            return;
+        }
+
+        $this->realLogger->log($level, $message, $context);
+    }
+}
---
+++ tests/Unit/Logger/FilteredLogLevelDecoratorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scoutapm\UnitTests\Logger;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Scoutapm\Logger\FilteredLogLevelDecorator;
+use function uniqid;
+
+/** @covers \Scoutapm\Logger\FilteredLogLevelDecorator */
+final class FilteredLogLevelDecoratorTest extends TestCase
+{
+    /** @var LoggerInterface|MockObject */
+    private $decoratedLogger;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->decoratedLogger = $this->createMock(LoggerInterface::class);
+    }
+
+    public function testLogMessagesBelowThresholdAreNotLogged() : void
+    {
+        $decorator = new FilteredLogLevelDecorator($this->decoratedLogger, LogLevel::NOTICE);
+
+        $this->decoratedLogger
+            ->expects(self::never())
+            ->method('log');
+
+        $decorator->info(uniqid('logMessage', true));
+    }
+
+    public function testLogMessagesAboveThresholdAreLogged() : void
+    {
+        $decorator = new FilteredLogLevelDecorator($this->decoratedLogger, LogLevel::NOTICE);
+
+        $logMessage = uniqid('logMessage', true);
+        $context    = [uniqid('foo', true) => uniqid('bar', true)];
+
+        $this->decoratedLogger
+            ->expects(self::once())
+            ->method('log')
+            ->with(LogLevel::WARNING, $logMessage, $context);
+
+        $decorator->warning($logMessage, $context);
+    }
+}
