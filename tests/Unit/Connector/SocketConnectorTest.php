dfe40596e8d5944b55154c4b8cc663b94be078d2
James Titcumb <james@asgrim.com>
2019-12-17 16:19:19 +0000

    Fixed constructor change for SocketConnector

--- tests/Unit/Connector/SocketConnectorTest.php
+++ tests/Unit/Connector/SocketConnectorTest.php
@@ -13,7 +13,7 @@ final class SocketConnectorTest extends TestCase
 {
     public function testExceptionIsRaisedWhenConnectingToNonExistentSocket() : void
     {
-        $connector = new SocketConnector('/path/does/not/exist');
+        $connector = new SocketConnector('/path/does/not/exist', false);
 
         $this->expectException(FailedToConnect::class);
         $this->expectExceptionMessage('socket_connect(): unable to connect [2]: No such file or directory');
