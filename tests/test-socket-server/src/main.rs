42f31e727f0d52cd16a36f95dab76f2e4b7044a7
James Titcumb <james@asgrim.com>
2021-06-09 10:28:14 +0100

    Updated test-socket-server to immediately exit when connection happens

--- tests/test-socket-server/src/main.rs
+++ tests/test-socket-server/src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{SocketAddrV4, Ipv4Addr, TcpListener};
-use std::io::{Read, Error};
+use std::io::Error;
 use std::env;
 
 fn main() -> Result<(), Error> {
@@ -14,10 +14,7 @@ fn main() -> Result<(), Error> {
     let listener = TcpListener::bind(socket)?;
     let port = listener.local_addr()?;
     println!("Listening on {}, access this port to end the program", port);
-    let (mut tcp_stream, addr) = listener.accept()?; //block  until requested
-    println!("Connection received! {:?} is sending data.", addr);
-    let mut input = String::new();
-    let _ = tcp_stream.read_to_string(&mut input)?;
-    println!("{:?} says {}", addr, input);
+    let (_tcp_stream, addr) = listener.accept()?; //block  until requested
+    println!("Connection received! {:?} - exiting", addr);
     Ok(())
 }
