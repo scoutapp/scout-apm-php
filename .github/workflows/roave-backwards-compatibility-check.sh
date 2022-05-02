b159ded2230b561b9af3a6a4b89473e242d073b3
James Titcumb <james@asgrim.com>
2022-05-02 12:17:16 +0100

    Adjust BC breaks

--- .github/workflows/roave-backwards-compatibility-check.sh
+++ .github/workflows/roave-backwards-compatibility-check.sh
@@ -13,9 +13,10 @@ echo "Running BC check, please wait..."
 OUTPUT=$(vendor/bin/roave-backward-compatibility-check --format=markdown "$@" 2>&1)
 
 # Remove rows we want to suppress
-OUTPUT=`echo "$OUTPUT" | sed '/Roave\\\BetterReflection\\\Reflection\\\ReflectionClass "Symfony\\\Component\\\HttpKernel\\\Event\\\FilterControllerEvent" could not be found in the located source/'d`
-OUTPUT=`echo "$OUTPUT" | sed '/Roave\\\BetterReflection\\\Reflection\\\ReflectionClass "Scoutapm\\\ScoutApmBundle\\\Twig\\\TwigMethods" could not be found in the located source/'d`
-OUTPUT=`echo "$OUTPUT" | sed '/Value of constant Twig\\\Environment::.* changed from .* to .*/'d`
+#OUTPUT=`echo "$OUTPUT" | sed '/Roave\\\BetterReflection\\\Reflection\\\ReflectionClass "Symfony\\\Component\\\HttpKernel\\\Event\\\FilterControllerEvent" could not be found in the located source/'d`
+#OUTPUT=`echo "$OUTPUT" | sed '/Roave\\\BetterReflection\\\Reflection\\\ReflectionClass "Scoutapm\\\ScoutApmBundle\\\Twig\\\TwigMethods" could not be found in the located source/'d`
+#OUTPUT=`echo "$OUTPUT" | sed '/Value of constant Twig\\\Environment::.* changed from .* to .*/'d`
+OUTPUT=`echo "$OUTPUT" | sed '/Method Illuminate\\\Support\\\Facades\\\Facade::.*() was removed/'d`
 
 # Number of rows we found with "[BC]" in them
 BC_BREAKS=`echo "$OUTPUT" | grep -o '\[BC\]' | wc -l | awk '{ print $1 }'`
@@ -23,7 +24,7 @@ BC_BREAKS=`echo "$OUTPUT" | grep -o '\[BC\]' | wc -l | awk '{ print $1 }'`
 # The last row of the output is "X backwards-incompatible changes detected". Find X.
 STATED_BREAKS=`echo "$OUTPUT" | tail -n 1 | awk -F' ' '{ print $1 }'`
 
-EXPECTED_STATED_BREAKS=21
+EXPECTED_STATED_BREAKS=2
 
 echo "$OUTPUT"
 echo "Lines with [BC] not filtered: $BC_BREAKS"
