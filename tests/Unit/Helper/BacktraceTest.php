f1b43beb8e45d4e4e7288f07666df2b3deb148be
James Titcumb <james@asgrim.com>
2022-06-01 11:55:25 +0200

    Ensure backtrace uses platform-agnostic DIRECTORY_SEPARATOR

--- tests/Unit/Helper/BacktraceTest.php
+++ tests/Unit/Helper/BacktraceTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use Scoutapm\Helper\Backtrace;
 
 use function array_keys;
+use function json_encode;
 
 /** @covers \Scoutapm\Helper\Backtrace */
 final class BacktraceTest extends TestCase
@@ -26,8 +27,9 @@ final class BacktraceTest extends TestCase
 
     public function testCapturingBacktraceFiltersOutVendor(): void
     {
+        $backtrace = Backtrace::captureWithoutVendor(0);
         // Since all Scoutapm stuff is already filtered out AND we're filtering vendor, this stack trace is actually
         // empty. This wouldn't happen if we're installed as a library, so is just a quirk of running this test
-        self::assertCount(0, Backtrace::captureWithoutVendor(0));
+        self::assertCount(0, $backtrace, 'Expected empty backtrace, actual value: ' . json_encode($backtrace));
     }
 }
